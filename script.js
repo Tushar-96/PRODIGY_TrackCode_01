762f8968b7e5e4e7da3d6d94724708b86c45bbb8
Tushar Sakharkar <117393527+Tushar-96@users.noreply.github.com>
2024-07-17 17:23:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+/* script.js */
+document.addEventListener('DOMContentLoaded', function() {
+  const navbar = document.getElementById('navbar');
+
+  window.addEventListener('scroll', function() {
+    if (window.scrollY > 50) {
+      navbar.style.backgroundColor = '#222';
+    } else {
+      navbar.style.backgroundColor = '#333';
+    }
+  });
+});
